ef9886f0e5e45c4003ad3ca2370a500d95d4ae22
Felipe6969 <rapmambru@hotmail.com>
2024-07-07 13:25:06 -0500

    Adicion de funcionalidad de desencriptado y finalización de paginas web de curriculum y about del proyecto para version final

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ function encriptar() {
         let parrafo = document.getElementById("parrafo"); 
         let avatar = document.getElementById("avatar");  
 
-
         let textoCifrado = texto
-                            .replace (/e/gi, "enter")
-                            .replace (/i/gi, "imes")
-                            .replace (/a/gi, "ai")
-                            .replace (/o/gi, "ober")
-                            .replace (/u/gi, "ufat");
-
+                            .replace (/e/gi, "ketib")
+                            .replace (/i/gi, "Iohar")
+                            .replace (/a/gi, "ari")
+                            .replace (/o/gi, "oryan")
+                            .replace (/u/gi, "umet");
  if (texto.length != 0) {
     document.getElementById ("texto").value = textoCifrado;
     tituloMensaje.textContent = "Texto encriptado con éxito";
@@ -23,5 +21,31 @@ function encriptar() {
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
     alert("debes ingresar algún texto");
  }
+}
+
+function desencriptar () {
+    let texto = document.getElementById("texto").value
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo"); 
+    let avatar = document.getElementById("avatar");  
+
+    let textoCifrado = texto
+    .replace(/ketib/gi, "e")
+    .replace (/Iohar/gi, "i")
+    .replace (/ari/gi, "a")
+    .replace (/oryan/gi, "o")
+    .replace (/umet/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById ("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        avatar.src = "./img/desencriptado.png";   
+    } else {
+        avatar.src ="./img/muneco con fondo.png"
+        tituloMensaje.textcontent = "Ningún mensaje fue encontrado"
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+        alert("debes ingresar algún texto"); 
+    }
 }
 
